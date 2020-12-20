5de9e7fc2571e4b94aa3e498b6b1915613d65d69
Chris Free <cfree@iMac-4.local>
2020-12-20 13:35:26 -0800

    tried to fix sysdata

--- R/build_package_data.R
+++ R/build_package_data.R
@@ -7,4 +7,4 @@
 spp_key <- read.csv("data/california_species_key.csv", as.is=T)
 
 # Save data for internal use (users can't see)
-usethis::use_data(spp_key, internal = TRUE)
+usethis::use_data(spp_key, internal = TRUE, overwrite = T)
