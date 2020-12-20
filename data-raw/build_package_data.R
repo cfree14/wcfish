f11da1be7d4f3b5a22fba8c4eff3b023e27d930d
Chris Free <cfree@iMac-4.local>
2020-12-20 13:42:37 -0800

    fixes

--- data-raw/build_package_data.R
+++ data-raw/build_package_data.R
@@ -4,7 +4,7 @@
 # https://r-pkgs.org/data.html
 
 # Read data
-spp_key <- read.csv("data/california_species_key.csv", as.is=T)
+spp_key <- read.csv("data-raw/california_species_key.csv", as.is=T)
 
 # Save data for internal use (users can't see)
 usethis::use_data(spp_key, internal = TRUE, overwrite = T)
