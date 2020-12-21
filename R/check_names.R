a7981128597204ada2e66558536b4abd682dbbf5
Chris Free <cfree@iMac-4.local>
2020-12-21 07:38:40 -0800

    added recode functionality

--- R/check_names.R
+++ R/check_names.R
@@ -4,12 +4,13 @@
 #' This function checks the spelling of common names for West Coast fish species. It aims to help harmonize datasets based on common names.
 #'
 #' @param names A character vector of species common names
-#' @return A data frame showing suggestions for misspelled common names
+#' @param return 1 returns a list formatted for recoding and 2 returns a data frame
+#' @return A list or data frame showing suggestions for misspelled common names
 #' @examples
 #' # Suggest spelling corrections for common names
-#' check_names(names=c("Pacific codd", "CA spiny lobster", "Dungeness crab"))
+#' check_names(names=c("Pacific codd", "CA spiny lobster", "Dungeness crab"), return=2)
 #' @export
-check_names <- function(names){
+check_names <- function(names, return=1){
 
   # Identify unique species
   spp_vec <- sort(unique(names))
@@ -22,7 +23,13 @@ check_names <- function(names){
 
   # Identify the best alternatives
   key <- data.frame(comm_name_orig=spp_vec_bad,
-                    comm_name=spp_vec_good, row.names = NULL)
+                    comm_name=spp_vec_good, row.names = NULL) %>%
+    dplyr::mutate(recode_text=paste0("'", comm_name_orig, "'='", comm_name, "'"))
+
+  # If return 1
+  if(return==1){
+    key <- paste(key$recode_text, collapse=", ")
+  }
 
   # Return
   return(key)
