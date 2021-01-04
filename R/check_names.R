f7404c527c1d844cfb95adbbb391ad1f3d496a52
Chris Free <cfree@iMac-4.local>
2021-01-04 08:30:46 -0800

    added procedure for when all names are right

--- R/check_names.R
+++ R/check_names.R
@@ -18,17 +18,27 @@ check_names <- function(names, return=1){
   # Which names are not correct?
   spp_vec_bad <- spp_vec[!spp_vec %in% spp_key$comm_name]
 
-  # Identify correct names
-  spp_vec_good <- sapply(spp_vec_bad, function(x) spp_key$comm_name[agrep(x, spp_key$comm_name)][1])
+  # If bad names
+  if(length(spp_vec_bad)>0){
 
-  # Identify the best alternatives
-  key <- data.frame(comm_name_orig=spp_vec_bad,
-                    comm_name=spp_vec_good, row.names = NULL) %>%
-    dplyr::mutate(recode_text=paste0("'", comm_name_orig, "'='", comm_name, "'"))
+    # Identify correct names
+    spp_vec_good <- sapply(spp_vec_bad, function(x) spp_key$comm_name[agrep(x, spp_key$comm_name)][1])
 
-  # If return 1
-  if(return==1){
-    key <- paste(key$recode_text, collapse=", ")
+    # Identify the best alternatives
+    key <- data.frame(comm_name_orig=spp_vec_bad,
+                      comm_name=spp_vec_good, row.names = NULL) %>%
+      dplyr::mutate(recode_text=paste0("'", comm_name_orig, "'='", comm_name, "'"))
+
+    # If return 1
+    if(return==1){
+      key <- paste(key$recode_text, collapse=", ")
+    }
+
+  }else{
+
+    key <- NULL
+    message("All names are correct.")
+
   }
 
   # Return
