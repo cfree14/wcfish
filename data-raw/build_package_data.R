5354ed129e4518297f2a1b8f937cb5fcbabace3a
Chris Free <cfree@iMac-4.local>
2021-01-20 20:00:26 -0800

    expanded species list

--- data-raw/build_package_data.R
+++ data-raw/build_package_data.R
@@ -4,7 +4,9 @@
 # https://r-pkgs.org/data.html
 
 # Read data
-spp_key <- read.csv("data-raw/california_species_key.csv", as.is=T)
+spp_key <- read.csv("data-raw/california_species_key.csv", as.is=T) %>%
+  arrange(type, comm_name) %>%
+  unique()
 
 # Save data for internal use (users can't see)
 usethis::use_data(spp_key, internal = TRUE, overwrite = T)
