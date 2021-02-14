6abc38368c318c6b1a067e24a71a2e909e263d9c
Chris Free <cfree@iMac-4.local>
2021-02-14 08:11:06 -0800

    updated check names to check/suggest reversed names

--- R/check_names.R
+++ R/check_names.R
@@ -8,21 +8,26 @@
 #' @return A list or data frame showing suggestions for misspelled common names
 #' @examples
 #' # Suggest spelling corrections for common names
-#' check_names(names=c("Pacific codd", "CA spiny lobster", "Dungeness crab"), return=2)
+#' check_names(names=c("Pacific codd", "CA spiny lobster", "Dungeness crab", "Crab, Dungness"), return=2)
 #' @export
 check_names <- function(names, return=1){
 
   # Identify unique species
   spp_vec <- sort(unique(names))
 
+  # Build vector of good name
+  spp_good_reg <- spp_key$comm_name_orig
+  spp_good_rev <- wcfish::convert_names(spp_key$comm_name_orig, to="reverse")
+  spp_good_all <- c(spp_good_reg, spp_good_rev)
+
   # Which names are not correct?
-  spp_vec_bad <- spp_vec[!spp_vec %in% spp_key$comm_name_orig]
+  spp_vec_bad <- spp_vec[!spp_vec %in% spp_good_all]
 
   # If bad names
   if(length(spp_vec_bad)>0){
 
     # Identify correct names
-    spp_vec_good <- sapply(spp_vec_bad, function(x) spp_key$comm_name_orig[agrep(x, spp_key$comm_name_orig)][1])
+    spp_vec_good <- sapply(spp_vec_bad, function(x) spp_good_all[agrep(x, spp_good_all)][1])
 
     # Identify the best alternatives
     key <- data.frame(comm_name_orig=spp_vec_bad,
